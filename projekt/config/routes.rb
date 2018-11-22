1447e50b7ebf92bcb2150a455d73855d175fbca7
root <aikanshgarg0303@gmail.com>
2018-11-22 12:21:54 +0530

    User SignUp

--- projekt/config/routes.rb
+++ projekt/config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  
+	# modifying devise routes for adding the feature of user confirming their account through a link sent to their mail.
+	# How it works? 
+	# The person creating the team generates an account of his team mate with name and email and devise sends a link for same to that email address. Then, the user comes through that link and generates a password for his account which is then appended to the same account!
+
+	as :user do
+		put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+	end
+
+
+
+  devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmations: 'confirmations'
+  }
+
+  
 end
