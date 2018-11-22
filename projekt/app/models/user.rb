a6034a22211b21787425467a7507ff9eb7c0d2eb
root <aikanshgarg0303@gmail.com>
2018-11-22 12:50:49 +0530

    remove :trackable from devise

--- projekt/app/models/user.rb
+++ projekt/app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :projects
-  has_many :teams
+  # has_many :projects
+  # has_many :teams
 
   # new function to set the password without knowing the current
   # password used in our confirmation controller.
