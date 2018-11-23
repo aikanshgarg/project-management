83c64d5f1e883d992318481d84fbec63e4687607
root <aikanshgarg0303@gmail.com>
2018-11-23 23:43:17 +0530

    webpack-server-errorwebpack-dev-server: command not found

--- projekt/config/initializers/content_security_policy.rb
+++ projekt/config/initializers/content_security_policy.rb
@@ -30,4 +30,11 @@ Rails.application.config.content_security_policy do |policy|
   else
     policy.script_src :self, :https
   end
+	
+	if Rails.env.development?
+	  policy.script_src :self, :https, :unsafe_eval
+	else
+	  policy.script_src :self, :https
+	end
+
 end
