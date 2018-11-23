ce1fd035ab8533c83a37567ad0230f3fff1cbd06
root <aikanshgarg0303@gmail.com>
2018-11-23 11:33:27 +0530

    edited controllers

--- projekt/app/models/project.rb
+++ projekt/app/models/project.rb
@@ -3,5 +3,5 @@ class Project < ApplicationRecord
 	belongs_to :user
 
 	# user can be in multiple teams and still create a project in any team, as opposed to the fact that he needs to be in a certain team to create a project in that team!
-	accept_nested_attributes_for :team
+	accepts_nested_attributes_for :team
 end
--- projekt/app/models/team.rb
+++ projekt/app/models/team.rb
@@ -1,9 +1,9 @@
 class Team < ApplicationRecord
 
-	has_many :user
+	has_many :users
 	has_many :projects, dependent: :destroy
 
-	accept_nested_attributes_for :user, allow_destroy: true
+	accepts_nested_attributes_for :users, allow_destroy: true
 
 
 end
